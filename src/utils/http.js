c1f59ef576ef2de343f50df45594753901e94b5a
Rizqi Siti Inayah 2428250080P <rizqisitiinayah_2428250080p@mhs.mdp.ac.id>
2025-12-01 19:58:29 +0700

    commit 3.1

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL : "flask-model-diabetes-production-3157.up.railway.app/api",
+    baseURL : "https://flask-model-diabetes-production-3157.up.railway.app/api",
     headers : {
         Accept : "application/json"
     }
